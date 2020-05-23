00a56e0974eb9cfab77a448c670891879c8ee172
Caio Mantovani <caiovmborba@gmail.com>
2020-05-23 16:07:26 -0300

    Properties

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React from 'react';
 import Header from './components/Header';
 
 function App() {
-  return <Header />;
+  return <Header title="Homepage">
+    <ul>
+      <li>Login</li>
+      <li>Register</li>
+    </ul>
+  </Header>;
 }
 
 export default App;
