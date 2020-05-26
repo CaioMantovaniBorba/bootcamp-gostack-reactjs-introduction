c9094bcea3a1dc83826740476068f3fd29aaa8a5
Caio Mantovani <caiovmborba@gmail.com>
2020-05-25 21:10:44 -0300

    State and Immutability

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
-  return <Header title="Homepage">
-    <ul>
-      <li>Login</li>
-      <li>Register</li>
-    </ul>
-  </Header>;
-}
+  const [projects, setProjects] = useState(['Desenvolvimento de App', 'Front-end web']);
 
+  function handleAddProject() {
+    // projects.push(`Novo projeto ${Date.now()}`);
+
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    console.log(projects);
+  }
+
+  return (
+    <>
+      <Header title='Projects' />
+
+      <ul>
+        {projects.map(project =>
+          <li key={project}>{project}</li>
+        )}
+      </ul>
+
+      <button type='button' onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+  );
+}
 export default App;
