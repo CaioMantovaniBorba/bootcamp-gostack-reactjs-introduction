a806c451b999d9fa8ecd9a201f6f8622ae748120
Caio Mantovani <caiovmborba@gmail.com>
2020-05-26 04:47:51 -0300

    Register projects

--- src/App.js
+++ src/App.js
@@ -16,10 +16,15 @@ function App() {
     });
   }, [])
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Caio Mantovani Borba'
+    });
 
-    console.log(projects);
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
